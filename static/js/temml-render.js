8e1f155301ce24a59b6f190a44c9941919f67fbc
edoardob90 <edoardob90@gmail.com>
2023-08-21 22:45:41 +0200

    Rename

--- static/js/temml-render.js
+++ static/js/temml-render.js
@@ -2,8 +2,10 @@
 var mathElements = document.getElementsByTagName("math");
 
 // Render all the math elements
-for (let mathElem of [...mathElements]) {
+for (let mathElem of mathElements) {
   const texToRender = mathElem.textContent;
   const displayMode = mathElem.classList.contains("display");
-  temml.render(texToRender, mathElem, { displayMode });
+  // temml.render(texToRender, mathElem, { displayMode });
+  const mathText = temml.renderToString(texToRender, { displayMode });
+  mathElem.parentElement.innerHTML = mathText;
 }
