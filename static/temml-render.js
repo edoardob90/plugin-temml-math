13195a4397253ed6283ff2e000425154edc951e9
edoardob90 <edoardob90@gmail.com>
2023-08-21 17:46:01 +0200

    Update

---
+++ static/temml-render.js
@@ -0,0 +1,9 @@
+// Select all the elements inside a <math> tag
+var mathElements = document.getElementsByTagName("math");
+
+// Render all the math elements
+for (let mathElem of [...mathElements]) {
+  const texToRender = mathElem.textContent;
+  const displayMode = mathElem.classList.contains("display");
+  temml.render(texToRender, mathElem, { displayMode });
+}
